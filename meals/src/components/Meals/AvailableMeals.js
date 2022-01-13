66e062e6b5f10aff91b84d237d20f9d7c8cc296d
JoakimMarcus <joakim_h89@hotmail.com>
2022-01-13 09:06:25 +0100

    added error message

--- meals/src/components/Meals/AvailableMeals.js
+++ meals/src/components/Meals/AvailableMeals.js
@@ -7,12 +7,18 @@ import classes from "./AvailableMeals.module.css";
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
-        "https://react-9c3ba-default-rtdb.firebaseio.com/meals.json"
+        "https://react-9c3ba-default-rtdb.firebaseio.com/meals."
       );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -30,13 +36,24 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   if (isLoading) {
     return (
       <section className={classes.MealIsLoading}>
         <p>Loading</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealError}>
+        <p>{httpError}</p>
       </section>
     );
   }
