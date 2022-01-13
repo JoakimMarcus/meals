aeb879435a4095feae78bda01bc39afeb740ecc0
JoakimMarcus <joakim_h89@hotmail.com>
2022-01-13 09:34:53 +0100

    added form for checkout

--- meals/src/components/Meals/AvailableMeals.js
+++ meals/src/components/Meals/AvailableMeals.js
@@ -12,7 +12,7 @@ const AvailableMeals = () => {
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
-        "https://react-9c3ba-default-rtdb.firebaseio.com/meals."
+        "https://react-9c3ba-default-rtdb.firebaseio.com/meals.json"
       );
 
       if (!response.ok) {
